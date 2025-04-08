2b80fb66af838c999c28841985c83316a0a7a47a
Bohdan <pusanukbr@gmail.com>
2025-04-08 12:33:12 +0200

    feat: add SVG logo components and integrate them into the logo.vue file

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,42 +1,43 @@
-import { defineNuxtConfig } from "nuxt/config";
+import { defineNuxtConfig } from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   components: true,
   nitro: {
     routeRules: {
-      "/_nuxt/**": { headers: { "cache-control": "no-store, max-age=0" } },
-    },
+      '/_nuxt/**': { headers: { 'cache-control': 'no-store, max-age=0' } }
+    }
   },
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   modules: [
-    "@nuxtjs/i18n",
-    "@nuxtjs/seo",
-    "@vueuse/nuxt",
-    "@pinia/nuxt",
-    "@nuxt/fonts",
-    "@nuxt/icon",
-    "@nuxt/image",
+    '@nuxtjs/i18n',
+    '@nuxtjs/seo',
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    'nuxt-svgo'
   ],
   i18n: {
-    vueI18n: "./i18n.config.ts",
-    langDir: "./lang/", // ВАЖЛИВО: переконайся, що папка `lang/` існує
+    vueI18n: './i18n.config.ts',
+    langDir: './lang/', // ВАЖЛИВО: переконайся, що папка `lang/` існує
     lazy: true,
     restructureDir: false,
-    strategy: "no_prefix",
+    strategy: 'no_prefix',
     detectBrowserLanguage: {
       useCookie: true,
-      cookieKey: "i18n_redirected",
-      redirectOn: "root",
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root'
     },
     locales: [
-      { code: "en", name: "English", file: "en-US.js" },
-      { code: "uk", name: "Українська", file: "uk-UA.js" },
-    ],
+      { code: 'en', name: 'English', file: 'en-US.js' },
+      { code: 'uk', name: 'Українська', file: 'uk-UA.js' }
+    ]
   },
   fonts: {
-    families: [{ name: "Montserrat", provider: "google" }],
+    families: [{ name: 'Montserrat', provider: 'google' }]
   },
   image: {
     provider: 'ipx',
@@ -44,22 +45,22 @@ export default defineNuxtConfig({
   },
   icon: {
     serverBundle: {
-      collections: ["fluent"], // <!--- this
+      collections: ['fluent'] // <!--- this
     },
     customCollections: [
       // Changed from object to array
       {
-        prefix: "my-icon",
-        dir: "./assets/my-icons",
-      },
-    ],
+        prefix: 'my-icon',
+        dir: './assets/my-icons'
+      }
+    ]
   },
-  css: ["~/assets/scss/main.scss"],
+  css: ['~/assets/scss/main.scss'],
   vite: {
     server: {
       watch: {
-        usePolling: true,
-      },
+        usePolling: true
+      }
     },
     css: {
       preprocessorOptions: {
@@ -67,10 +68,10 @@ export default defineNuxtConfig({
           additionalData: `
             @use "~/assets/scss/_variables.scss" as *;
             @use "~/assets/scss/_mixins.scss" as *;
-           `,
-        },
-      },
-    },
+           `
+        }
+      }
+    }
   },
   app: {
     head: {
@@ -86,11 +87,14 @@ export default defineNuxtConfig({
               document.documentElement.classList.remove('dark');
             }
           })();`,
-          type: "text/javascript",
-          tagPosition: "head",
-        },
+          type: 'text/javascript',
+          tagPosition: 'head'
+        }
       ],
-      __dangerouslyDisableSanitizers: ["script"],
-    } as any,
+      __dangerouslyDisableSanitizers: ['script']
+    } as any
   },
-});
+  svgo: {
+    autoImportPath: './assets/my-icons/'
+  }
+})
